d45a6719323e1d8d34d24af981c289a7318c6ba3
Nilo Neregato <niloneregato@hotmail.com>
2019-09-08 00:13:06 +0200

    added product-routing module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,27 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { ProductListComponent } from '../product/product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ConverToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { StarComponent } from '../shared/start.component';
-import { ProductDetailGuard } from './product-detail.guard';
 import { SharedModule } from '../shared/shared.module';
+import { ProductRoutingModule } from './product-routing.module';
 
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id',
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent },
-    ]),
-    SharedModule
+    SharedModule,
+    ProductRoutingModule
   ],
   declarations: [
     ProductListComponent,
